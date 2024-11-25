b195d176f574b0f40c695b0986fa24e7eb636da4
gd2872 <gd2872@naver.com>
2024-11-25 13:18:55 +0900

    프로젝트 초기화

---
+++ src/main/java/com/example/nachojang/NachojangApplication.java
@@ -0,0 +1,13 @@
+package com.example.nachojang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NachojangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NachojangApplication.class, args);
+	}
+
+}
